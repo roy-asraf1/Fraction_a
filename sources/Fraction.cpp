cc26c438e94c81d64fb2616c377454b22c47ed7e
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2023-04-30 16:26:11 +0300

    Update Fraction.cpp

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -1,80 +1,30 @@
 
+
 #include "Fraction.hpp" 
 #include "iostream"
 using namespace std;
-using namespace ariel;
+ namespace ariel{
 
-Fraction :: Fraction (int numa, int numb){
-    this->Numerator =numa;
-    this->Denominator=numb;
+Fraction :: Fraction (int Numerator, int Denominator){
+    this->Numerator =Numerator;
+    this->Denominator=Denominator;
 }
-Fraction :: Fraction (double numa){
+Fraction :: Fraction (float numa){
     this->Numerator =numa;
     this->Denominator=1;
 }
 Fraction ::Fraction (){
     this->Numerator =0;
-    this->Denominator =0;
+    this->Denominator =1;
 }
 Fraction :: Fraction (const Fraction& copy):Numerator(copy.Numerator),Denominator(copy.Denominator){//copy constractor
 
 }
 Fraction :: ~Fraction(){
 
-    cout << "Destructor called" <<endl;
+   
 
 }
-
-Fraction Fraction :: operator+(const Fraction& other) const{
-    Fraction ans = (other.Numerator+Numerator, other.Denominator+Denominator);
-    return ans;
-}
-Fraction Fraction :: operator-(const Fraction &other) const{
-    int numerator = Numerator * other.Denominator - Denominator * other.Numerator;
-    int denominator = Denominator * other.Denominator;
-    return Fraction(numerator, denominator);
-}
-Fraction Fraction :: operator/(const Fraction &other) const{
-    int numerator = Numerator * other.Denominator;
-    int denominator = Denominator * other.Numerator;
-    return Fraction(numerator, denominator);
-
-
-}
-Fraction Fraction :: operator*(const Fraction &other) const{
-    int numerator = Numerator * other.Numerator;
-    int denominator = Denominator * other.Denominator;
-    return Fraction(numerator, denominator);
-
-}
-Fraction operator*(const double& d, const Fraction& f) {
-
-}
-Fraction& Fraction ::operator++(){
-    Numerator += Denominator;
-    return *this;
-    
-}
-Fraction& Fraction ::operator--(){
-    Numerator -= Denominator;
-    return *this;
-    
-}
-
-bool Fraction :: operator>(const Fraction& other)const{
-    return Numerator * other.Denominator > other.Numerator * Denominator;
-}
-bool Fraction :: operator<(const Fraction& other)const{
-    return Numerator * other.Denominator < other.Numerator * Denominator;
-}
-
-bool Fraction::operator>=(const Fraction& other) const {
-    Numerator * other.Denominator >= other.Numerator * Denominator;
-}
-bool Fraction :: operator<=(const Fraction& other)const{
-    return Numerator * other.Denominator <= other.Numerator * Denominator;
-}
-
 int Fraction :: getDenominator () const{
     return Denominator;
 }
@@ -82,13 +32,99 @@ int Fraction :: getNumerator () const{
     return Numerator;
 }
 
-std::ostream& operator<<(std::ostream& os, const Fraction& fraction)
-{
-    os << fraction.getNumerator() << "/" << fraction.getDenominator(); // output the fraction as numerator/denominator
-    return os;
-}
-std::istream& operator>>(std::istream& is, Fraction& fraction)
 
-{
+Fraction Fraction::operator+(Fraction& other){
+    Fraction ans = Fraction(other.Numerator + Numerator, other.Denominator + Denominator);
+    return ans;
+}
+float Fraction::operator+(float fractNumber){
+    float end = (this->Numerator / this->Denominator) + fractNumber;
+    return end;
+}
+Fraction Fraction::operator-(Fraction& other){
+    int numerator = Numerator * other.Denominator - Denominator * other.Numerator;
+    int denominator = Denominator * other.Denominator;
+    return Fraction(numerator, denominator);
+}
+float Fraction::operator-(float fractNumber){
+    float end = (this->Numerator / this->Denominator) - fractNumber;
+    return end;
+}
+Fraction Fraction::operator/(Fraction& other){
+    int numerator = Numerator * other.Denominator;
+    int denominator = Denominator * other.Numerator;
+    return Fraction(numerator, denominator);
+
 
 }
+Fraction Fraction::operator*(Fraction& other){
+    int numerator = Numerator * other.Numerator;
+    int denominator = Denominator * other.Denominator;
+    return Fraction(numerator, denominator);
+
+}
+    float Fraction::operator*(float fractNumber){
+        float end = (this->Numerator / this->Denominator) * fractNumber;
+        return end;
+}
+Fraction operator*(float fractNum, const Fraction &frac){
+    return Fraction();
+}
+bool Fraction::operator==(const Fraction& other) const { 
+    if (other.Numerator==Numerator && other.Denominator==Denominator)
+        return true;
+    return false;
+}
+
+bool Fraction :: operator>(const Fraction& other)const{
+    bool t =false;
+    if( Numerator * other.Denominator > other.Numerator * Denominator)
+        t=true;
+    return t;
+}
+bool Fraction :: operator<(const Fraction& other)const{
+    bool t =true;
+    if( Numerator * other.Denominator < other.Numerator * Denominator)
+        t=false;
+    return t;
+}
+
+Fraction Fraction ::operator++(){
+    Numerator += Denominator;
+    return *this;
+    
+}
+Fraction Fraction ::operator++(int){
+    Numerator += Denominator;
+    return *this;
+    
+}
+Fraction Fraction ::operator--(){
+    Numerator -= Denominator;
+    return *this;
+    
+}
+Fraction Fraction ::operator--(int){
+    Numerator -= Denominator;
+    return *this;
+    
+}
+
+bool Fraction::operator>=(const Fraction& other) const {
+    return Numerator * other.Denominator >= other.Numerator * Denominator;
+}
+bool Fraction :: operator<=(const Fraction& other)const{
+    return Numerator * other.Denominator <= other.Numerator * Denominator;
+}
+
+    std::ostream& operator<<(std::ostream& outs, const Fraction& fraction) {
+    
+    return outs;
+}
+std::istream& operator>>(std::istream& ins, Fraction& fraction) {
+    return ins;
+}
+void Fraction:: reduce() {
+    cout << "start reduce" <<endl;
+}
+ }
